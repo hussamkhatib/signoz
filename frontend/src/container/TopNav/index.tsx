ec326ea4ccd25fb8ca1a8a037d1a651f9fdf1e4e
hussamkhatib <hussamkhatib@gmail.com>
2022-10-25 19:20:35 +0530

    feat(FE): global time visibility as per route

--- frontend/src/container/TopNav/index.tsx
+++ frontend/src/container/TopNav/index.tsx
@@ -8,37 +8,35 @@ import ShowBreadcrumbs from './Breadcrumbs';
 import DateTimeSelector from './DateTimeSelection';
 import { Container } from './styles';
 
-const routesToSkip = [
-	ROUTES.SETTINGS,
-	ROUTES.LIST_ALL_ALERT,
-	ROUTES.TRACE_DETAIL,
-	ROUTES.ALL_CHANNELS,
-	ROUTES.USAGE_EXPLORER,
-	ROUTES.INSTRUMENTATION,
-	ROUTES.VERSION,
-	ROUTES.ALL_DASHBOARD,
-	ROUTES.ORG_SETTINGS,
-	ROUTES.ERROR_DETAIL,
-	ROUTES.ALERTS_NEW,
-	ROUTES.EDIT_ALERTS,
-	ROUTES.LIST_ALL_ALERT,
+const skipDateTimeSelectorRoutes = [
+	'/settings',
+	'/alerts',
+	'/trace/:id',
+	'/settings/channels',
+	'/usage-explorer',
+	'/get-started',
+	'/status',
+	'/dashboard',
+	'/settings/org-settings',
+	'/error-detail',
+	'/alerts/new',
+	'/alerts/edit',
+	'/alerts',
 ];
+
+const checkRouteExists = (currentPath: string): boolean =>
+	Boolean(
+		matchPath(currentPath, {
+			path: skipDateTimeSelectorRoutes,
+			exact: true,
+			strict: true,
+		}),
+	);
 
 function TopNav(): JSX.Element | null {
 	if (history.location.pathname === ROUTES.SIGN_UP) {
 		return null;
 	}
-
-	const checkRouteExists = (currentPath: string): boolean => {
-		for (let i = 0; i < routesToSkip.length; i += 1) {
-			if (
-				matchPath(currentPath, { path: routesToSkip[i], exact: true, strict: true })
-			) {
-				return true;
-			}
-		}
-		return false;
-	};
 
 	return (
 		<Container>
